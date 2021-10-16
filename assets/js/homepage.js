afce22bfd37d2ddade06d84f7c4c80618a8eaa29
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-10-15 20:21:17 -0700

    added langauages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -76,5 +77,30 @@ var getUserRepos = function(user){
     });
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+          
+        } else {
+          alert('Error: GitHub User Not Found');
+        }
+      });
+};
+
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+    if(language){
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = "";
+    };
+};
+
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
